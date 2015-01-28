a0fb26856e26ff9f666b6de46f1b8f4d1a5776fe
Sam Willcocks <sam@wlcx.cc>
2015-01-28 23:59:08 +0000

    Connects to localhost 1350 and prints any data received

--- ury-rapid-go.go
+++ ury-rapid-go.go
@@ -1,7 +1,22 @@
 package main
 
+import "os"
 import "fmt"
+import "net"
+import "bufio"
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	conn, err := net.Dial("tcp", "127.0.0.1:1350")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for {
+		data, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf(data)
+	}
 }
