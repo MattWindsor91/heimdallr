5eaad79c231015def8d6e0943ac869f3d42dd926
Sam Willcocks <sam@wlcx.cc>
2015-01-29 05:11:47 +0000

    Flesh out tokeniser a bit

--- tokeniser.go
+++ tokeniser.go
@@ -3,9 +3,9 @@ package tokeniser
 type QuoteType int
 
 const (
-	None   QuoteType = 0
-	Single QuoteType = 1
-	Double QuoteType = 2
+	None   QuoteType = iota
+	Single
+	Double
 )
 
 type Tokeniser struct {
@@ -16,11 +16,63 @@ type Tokeniser struct {
 	lines            [][]string
 }
 
+func (t *Tokeniser) Push(b byte) {
+    t.word.push(b)
+    t.escape_next_char = false
+}
+
+func (t *Tokeniser) EndLine() {
+    break
+}
+
+
+func (t *Tokeniser) EndWord() {
+    break
+}
+
 func (t *Tokeniser) Parse(data []byte) {
 	for _, b := range data {
 		if t.escape_next_char {
+            // TODO: Make unicode safe
 			t.word.append(b)
 			t.escape_next_char = true
 		}
-	}
+        switch t.quote_type {
+        case None:
+            switch b {
+            case '\'':
+                t.quote_type = None
+            case '"':
+                t.quote_type = None
+            case '\\':
+                t.escape_next_char = true
+            case '\n':
+                t.EndLine()
+            default:
+                if b == ' ' {
+                    t.EndWord()
+                } else {
+                    t.Push(b)
+                }
+            }
+
+        case Single:
+            switch b {
+            case '\'':
+                t.quote_type = QuoteType.None
+            default:
+                t.Push(b)
+            }
+
+        case Double:
+            switch b {
+            case '"':
+                t.quote_type = None
+            case '\\':
+                t.escape_next_character = true
+            default:
+                t.Push(b)
+            }
+        }
+    }
 }
