1ac10c8b0ca28ff525bf460969a7f0774fe78560
Sam Willcocks <sam@wlcx.cc>
2015-01-31 03:04:21 +0000

    Add packer and tests

---
+++ tokeniser/pack.go
@@ -0,0 +1,23 @@
+package tokeniser
+
+import (
+	"bytes"
+	"strings"
+)
+
+func Pack(word string, args []string) []byte {
+	output := new(bytes.Buffer)
+	// TODO: check command word is valid
+	output.WriteString(word)
+	for _, a := range args {
+		if strings.ContainsAny(a, "'\"\\ ") { // Does this arg contain special characters?
+			a = escapeArgument(a)
+		}
+		output.WriteString(" " + a)
+	}
+	return output.Bytes()
+}
+
+func escapeArgument(input string) (output string) {
+	return "'" + strings.Replace(input, "'", "'\\''", -1) + "'"
+}
