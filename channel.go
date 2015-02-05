d015fd07ab2e41ac4be7504ddad8182f77b1c4d0
Sam Willcocks <sam@wlcx.cc>
2015-02-05 00:41:54 +0000

    Channels now communicate with channels (yo dawg)
    This involves:
    Channels now spin up a goroutine to deal with the tcp connection
    This sends tokenised lines back to the main channel Run() loop
    Which selects (currently) on received lines and any errors
    Channels now accept two (go) channels for requests/responses in their 'constructor'
    Requests go nowhere currently.
    The name channel is getting confusing. Rename inbound.
    In addition:
    Factor out time.Duration formatting into utils.go

--- channel.go
+++ channel.go
@@ -14,9 +14,11 @@ type Channel struct {
 	tokeniser *baps3protocol.Tokeniser
 	conn      net.Conn
 	buf       *bufio.Reader
+	resCh     chan<- string
+	reqCh     <-chan string
 }
 
-func InitChannel(hostport string) *Channel {
+func InitChannel(hostport string, reqCh <-chan string, resCh chan<- string) *Channel {
 	c := new(Channel)
 	c.tokeniser = baps3protocol.NewTokeniser()
 	conn, err := net.Dial("tcp", hostport)
@@ -26,29 +28,49 @@ func InitChannel(hostport string) *Channel {
 	}
 	c.conn = conn
 	c.buf = bufio.NewReader(c.conn)
+	c.resCh = resCh
+	c.reqCh = reqCh
 	return c
 }
 
 func (c *Channel) Run() {
+	lineCh := make(chan [][]string, 3)
+	errCh := make(chan error)
+
+	// Spin up a goroutine to accept and tokenise incoming bytes, and spit them
+	// out in a channel
+	go func(lineCh chan [][]string, eCh chan error) {
+		for {
+			data, err := c.buf.ReadBytes('\n')
+			if err != nil {
+				errCh <- err
+			}
+			lineCh <- c.tokeniser.Tokenise(data)
+		}
+	}(lineCh, errCh)
+
+	// Main run loop, select on new received lines, errors or incoming requests
 	for {
-		data, err := c.buf.ReadBytes('\n')
-		if err != nil {
+		select {
+		case lines := <-lineCh:
+			for _, line := range lines {
+				switch line[0] {
+				case "TIME":
+					time, err := time.ParseDuration(line[1] + `us`)
+					if err != nil {
+						fmt.Println(err)
+						os.Exit(1)
+					}
+					c.time = time
+					c.resCh <- PrettyDuration(time)
+				case "STATE":
+					c.state = line[1]
+					c.resCh <- line[1]
+				}
+			}
+		case err := <-errCh:
 			fmt.Println(err)
 			os.Exit(1)
-		}
-		lines := c.tokeniser.Tokenise(data)
-		for _, line := range lines {
-			switch line[0] {
-			case "TIME":
-				time, err := time.ParseDuration(line[1] + "us")
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
-				c.time = time
-			case "STATE":
-				c.state = line[1]
-			}
 		}
 	}
 }
