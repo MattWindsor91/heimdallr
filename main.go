442b56bbf5cf45f6a738f4842b65efa60578b736
Sam Willcocks <sam@wlcx.cc>
2015-02-06 01:15:25 +0000

    Add logging and initial config file example + parsing

--- main.go
+++ main.go
@@ -4,8 +4,25 @@ import "os"
 import "os/signal"
 import "syscall"
 import "fmt"
+import "log"
+import "io/ioutil"
+
+import "github.com/BurntSushi/toml"
+
+type Config struct {
+	servers []string
+}
 
 func main() {
+	logger := log.New(os.Stdout, "[-] ", log.Lshortfile)
+	var conf Config
+	conffile, err := ioutil.ReadFile("conf_example.toml")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	if _, err := toml.Decode(string(conffile), &conf); err != nil {
+		logger.Fatal(err)
+	}
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT)
 	c1ReqCh := make(chan string)
