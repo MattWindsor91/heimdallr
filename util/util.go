e57b1d140d17e982bcd7063d86e3db7907b76b0d
mattbw <matt.windsor@ury.york.ac.uk>
2015-02-14 22:06:08 +0000

    Comment PrettyDuration.

--- util/util.go
+++ util/util.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+// PrettyDuration pretty-prints a duration in the form minutes:seconds.
+// The seconds part is zero-padded; the minutes part is not.
 func PrettyDuration(dur time.Duration) string {
 	return fmt.Sprintf("%d:%02d", int(dur.Minutes()), int(math.Mod(dur.Seconds(), 60)))
 }
