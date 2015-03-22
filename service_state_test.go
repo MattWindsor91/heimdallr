81ab883016049335c7ac54b164e6ccaa652ccbad
mattbw <matt.windsor@ury.york.ac.uk>
2015-03-22 15:23:19 +0000

    Add rudimentary error checks.

--- service_state_test.go
+++ service_state_test.go
@@ -7,7 +7,47 @@ import (
 	"github.com/UniversityRadioYork/baps3-go"
 )
 
-// TestServiceStateUpdate tests the updating of a ServiceState by messages.
+// TestServiceStateUpdateFail tests the behaviour of a serviceState when it
+// receives a malformed message.
+func TestServiceStateUpdateFail(t *testing.T) {
+	// TODO(CaptainHayashi): maybe test what the error actually is
+	cases := []struct {
+		msg    *baps3.Message
+		hasErr bool
+	}{
+		// Request where response was expected
+		{
+			baps3.NewMessage(baps3.RqLoad).AddArg("/quux"),
+			false, // TODO(CaptainHayashi): error on requests?
+		},
+		// Too few arguments
+		{
+			baps3.NewMessage(baps3.RsFile),
+			true,
+		},
+		// Too many arguments
+		{
+			baps3.NewMessage(baps3.RsTime).AddArg("3003").AddArg("lol"),
+			true,
+		},
+		// Unknown request (should be ignored)
+		{
+			baps3.NewMessage(baps3.RsUnknown).AddArg("heh"),
+			false,
+		},
+	}
+
+	for _, c := range cases {
+		err := initServiceState().update(*c.msg)
+		if c.hasErr && (err == nil) {
+			t.Errorf("expected %q to produce error, none produced", c.msg)
+		} else if !c.hasErr && (err != nil) {
+			t.Errorf("expected %q not to produce error, one produced", c.msg)
+		}
+	}
+}
+
+// TestServiceStateUpdate tests the updating of a serviceState by messages.
 func TestServiceStateUpdate(t *testing.T) {
 	// TODO(CaptainHayashi): test failure states as well as successes
 
