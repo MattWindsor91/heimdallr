706e641d874ea5e9a6ac0a41fc08159f0647e1fe
Sam Willcocks <sam@wlcx.cc>
2015-04-25 03:29:35 +0100

    Rehash config struct
    Adds service definitions and groups
    Renames a few things
    Update main to act as before but take the last service in a service group as the top of a stack

--- main.go
+++ main.go
@@ -15,18 +15,26 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
-type server struct {
-	Hostport string
+type service struct {
+	Svc          string
+	Device       int
+	Host, Dshost string
+	Port, Dsport int
 }
 
-type httpServer struct {
-	Hostport string
+type group struct {
+	name     string
+	services []service
+}
+
+type httpConf struct {
+	Listen string
 }
 
 // Config is a struct containing the configuration for an instance of Bifrost.
 type Config struct {
-	Servers map[string]server
-	HTTP    httpServer
+	Groups []group
+	HTTP   httpConf
 }
 
 func killConnectors(connectors []*bfConnector) {
@@ -74,10 +82,11 @@ func main() {
 
 	wg := new(sync.WaitGroup)
 
-	for name, s := range conf.Servers {
-		c := initBfConnector(name, resCh, wg, logger)
+	for _, group := range conf.Groups {
+		c := initBfConnector(group.name, resCh, wg, logger)
 		connectors = append(connectors, c)
-		c.conn.Connect(s.Hostport)
+		lastsvc := group.services[len(group.services)-1] // Bifrost connects to the last svc in a group
+		c.conn.Connect(fmt.Sprintf("%s:%d", lastsvc.Host, lastsvc.Port))
 		go c.Run()
 	}
 
@@ -103,10 +112,10 @@ func main() {
 	}
 }
 
-func initAndStartHTTP(conf httpServer, connectors []*bfConnector, wspool *Wspool, logger *log.Logger) {
+func initAndStartHTTP(conf httpConf, connectors []*bfConnector, wspool *Wspool, logger *log.Logger) {
 	mux := initHTTP(connectors, wspool, logger)
 	go func() {
-		err := http.ListenAndServe(conf.Hostport, mux)
+		err := http.ListenAndServe(conf.Listen, mux)
 		if err != nil {
 			logger.Println(err)
 		}
